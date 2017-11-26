240f56cce54ea4617c8be846ac9b09e0b49b1b68
Peter Coughlin <coughlin1966@gmail.com>
2017-11-26 00:10:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ function updateObjectWithKeyAndValue (list,ingrd,baked) {
   return Object.assign({}, list, {[ingrd]: baked} )
 }
 updateObjectWithKeyAndValue(recipes,eggs,'cake')
+
+
+
+
+
+
+
+
+
+function destructivelyUpdateObjectWithKeyAndValue (list,ingrd) {
+  var newlist = Object.assign ({},list)
+  delete newlist[ingrd]
+  return newlist
+}
+destructivelyUpdateObjectWithKeyAndValue(cake,'Vanilla')
